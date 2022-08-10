29ebcd79b18868ea49c65178260a64ab60eadf17
prathap045 <bunny110203@gmail.com>
2022-08-10 16:11:30 +0530

    prathap

---
+++ moksha/m.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('#nav-bar').addClass("sticky");
+        }else{
+            $('#nav-bar').removeClass("sticky");
+        }
+    });
+});
+
+
+//   
+   //Get the button
+      var mybutton = document.getElementById("myBtn");
+      
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function() {scrollFunction()};
+      
+      function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+      
+      // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+
+      // ----------------------------buutton function-------
+     
